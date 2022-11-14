5906c451ebc214840db4b503f0ebbc0a0bfc31ef
malinaalexa <104028370+malinaalexa@users.noreply.github.com>
2022-11-14 21:39:01 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
 //Steica Malina-Alexa, grupa 161
 
 #include <iostream>
-#include <string>
-#include <vector>
-#include <cstring>
+
 
 class Date
 {
@@ -15,9 +13,9 @@ public:
     Date (const std::string& nume_,
           const std::string& numar_telefon_,
           const std::string& adresa_mail_) :
-        nume{nume_},
-        numar_telefon{numar_telefon_},
-        adresa_mail{adresa_mail_}
+            nume{nume_},
+            numar_telefon{numar_telefon_},
+            adresa_mail{adresa_mail_}
     {
         std::cout<<"-Constructor Date-"<<'\n';
     };
@@ -26,6 +24,7 @@ public:
         os <<Date.nume<<" "<<Date.numar_telefon<<" "<<Date.adresa_mail;
         return os;
     }
+    friend class Produs;
 };
 class Distribuitor
 {
@@ -35,9 +34,9 @@ class Distribuitor
 public:
     Distribuitor (int id_distr_,const Date& date_distr_,
                   const std::string& adresa_) :
-        id_distr{id_distr_},
-        date_distr{date_distr_},
-        adresa{adresa_}
+            id_distr{id_distr_},
+            date_distr{date_distr_},
+            adresa{adresa_}
     {
         std::cout<<"-Constructor Distribuitor-"<<'\n';
     };
@@ -46,6 +45,7 @@ public:
         os <<Distr.id_distr<<" "<<Distr.date_distr<<" "<<Distr.adresa<<'\n';
         return os;
     }
+    friend class Produs;
 };
 class Produs
 {
@@ -56,6 +56,8 @@ class Produs
     std::string culoare;
     std::string marime;
     Distribuitor producator;
+    int nr_buc;
+    int redus=0;
 public:
     Produs(int id_prod_,
            const std::string& denumire_,
@@ -63,84 +65,110 @@ public:
            const std::string& material_,
            const std::string& culoare_,
            const std::string& marime_,
-           const Distribuitor &producator_) :
-        id_prod{id_prod_},
-        denumire{denumire_},
-        pret{pret_},
-        material{material_},
-        culoare{culoare_},
-        marime{marime_},
-        producator{producator_}
+           const Distribuitor &producator_,
+           int nr_buc_):
+            id_prod{id_prod_},
+            denumire{denumire_},
+            pret{pret_},
+            material{material_},
+            culoare{culoare_},
+            marime{marime_},
+            producator{producator_},
+            nr_buc{nr_buc_}
     {
         std::cout<<"-Constructor Produs-"<<'\n';
     };
 
     friend std::ostream& operator<<(std::ostream& os, const Produs& Prod)
     {
-        os <<Prod.id_prod<<" "<<Prod.denumire<<" "<<Prod.pret<<"lei "<<Prod.material<<" "<<Prod.culoare<<" "<<Prod.marime<<" "<<Prod.producator<<'\n';
+        os <<Prod.id_prod<<" "<<Prod.denumire<<" "<<Prod.pret<<"lei "<<Prod.material<<" "<<Prod.culoare<<" "<<Prod.marime<<" "<<Prod.producator<<Prod.nr_buc<<'\n';
         return os;
     }
-    friend void reducere (Produs &x);
-    friend void set_price(Produs &x);
+    void reducere ()
+    {
+        if(pret>80 && redus<=0)
+            {pret=pret-pret*0.2;
+            redus++;}
+    }
+    void lichidare_de_stoc()
+    {
+        if(nr_buc<10)
+        {
+            std::cout<<"Promotie! Pretul este redus cu 20% de doua ori!"<<'\n';
+            redus=-1;
+            reducere();
+            reducere();
+        }
+    }
+    void aprovizionare()
+    {
+        if((nr_buc<30 && nr_buc>10) || nr_buc==0)
+        {
+            std::cout<<"Vom contacta distribuitorul "<<producator.date_distr.nume<<" pentru a comanda "<<30-nr_buc<<" produse."<<'\n';
+            std::cout<<"Mail: "<<producator.date_distr.adresa_mail<<'\n'<<"Numar de telefon: "<<producator.date_distr.numar_telefon<<'\n';
+            nr_buc=30;
+        }
+    }
+
 };
 class Angajat
 {
     int id_angajat;
+    double salariu;
     std::string rol;
     Date date_personale;
 public:
     Angajat(int id_angajat_,
+            double salariu_,
             const std::string& rol_,
             const Date &date_personale_) :
-        id_angajat{id_angajat_},
-        rol{rol_},
-        date_personale{date_personale_}
+            id_angajat{id_angajat_},
+            salariu{salariu_},
+            rol{rol_},
+            date_personale{date_personale_}
     {
         std::cout<<"-Constructor Angajati-"<<'\n';
     };
     friend std::ostream& operator<<(std::ostream& os, const Angajat& An)
     {
-        os <<An.id_angajat<<" "<<An.rol<<" "<<An.date_personale<<'\n';
+        os <<An.id_angajat<<" "<<An.salariu<<" "<<An.rol<<" "<<An.date_personale<<'\n';
         return os;
     }
-    Angajat(const Angajat& other) : id_angajat{other.id_angajat}, rol{other.rol},date_personale{other.date_personale}
+    Angajat(const Angajat& other) : id_angajat{other.id_angajat}, salariu{other.salariu}, rol{other.rol},date_personale{other.date_personale}
     {
         std::cout<<"Constructor de copiere Angajat\n";
     }
     Angajat& operator=(const Angajat& other)
     {
         id_angajat = other.id_angajat;
+        salariu=other.salariu;
         rol=other.rol;
         date_personale=other.date_personale;
         return *this;
     }
+    void marire_salariu()
+    {
+        if(salariu<2000)
+            salariu=salariu+salariu*0.2;
+    }
     ~Angajat() {};//destructor
 
 };
-void reducere (Produs &x)
-{
-    if(x.pret>100)
-        x.pret=x.pret-x.pret*0.3;
-}
-void set_price(Produs &x)
-{if(x.denumire=="Bluza") x.pret=79.99;
-if(x.denumire=="Pantaloni") x.pret=129.99;
-if(x.denumire=="Rochie") x.pret=99.90;
-}
+
 int main()
 {
-    Angajat A1{1,"Manager",Date{"Andrei","0766555222","andrei@gmail.com"}};
+    Angajat A1{1,2300,"Manager",Date{"Andrei","0766555222","andrei@gmail.com"}};
+    Angajat A2{2,1560,"Agent de vanzari",Date{"Alex","0766555333","alex@gmail.com"}};
     Distribuitor D1{1,Date{"Zara","0766555333","zara@gmail.com"},"Strada 1 Decembrie, nr. 1"};
-    Produs P1{1,"Bluza",-1,"Matase","Alb","M",{D1}};
-    Produs P2{1,"Pantaloni",-1,"Piele","Negru","M",{D1}};
-    Produs P3{1,"Rochie",-1,"Satin","Rosu","M",{D1}};
-    std::cout<<A1<<D1<<P1<<P2<<P3;
-    set_price(P1);
-    set_price(P2);
-    set_price(P3);
-    std::cout<<P1<<P2<<P3;
-    reducere(P1);
-    reducere(P2);
-    std::cout<<"Dupa reducere"<<P1<<P2<<P3;
+    Produs P1{1,"Bluza",99.99,"Matase","Alb","M",{D1},0};
+    Produs P2{2,"Pantaloni",149.99,"Piele","Negru","M",{D1},15};
+    Produs P3{3,"Rochie",199.99,"Satin","Rosu","M",{D1},5};
+    std::cout<<A1<<A2<<D1<<P1<<P2<<P3;
+    A1.marire_salariu();
+    P2.reducere();
+    P1.aprovizionare();
+    P2.aprovizionare();
+    P3.lichidare_de_stoc();
+    std::cout<<P1<<P2<<P3<<A1;
     return 0;
 }
