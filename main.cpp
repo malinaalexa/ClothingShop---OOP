646ba44b0b20a46858e3f46d38ecafa69b637941
malinaalexa <104028370+malinaalexa@users.noreply.github.com>
2023-01-03 19:34:09 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@
 #include "comanda.h"
 
 int main() {
-    std::vector<Produs> prod;
-    prod.push_back(Produs(1,"Bluza",59.90,15));
-    prod.push_back(Produs(1,"Pantaloni",159.90,20));
-    prod.push_back(Produs(1,"Rochie",79.90,20));
+   // std::vector<Produs> prod;
+   // prod.push_back(Produs(1,"Bluza",59.90,15));
+   // prod.push_back(Produs(1,"Pantaloni",159.90,20));
+   // prod.push_back(Produs(1,"Rochie",79.90,20));
 
     return 0;
 }
