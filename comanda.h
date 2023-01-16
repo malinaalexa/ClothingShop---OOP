c70e7edc0d274b4244476f263dc266b651aea21c
malinaalexa <104028370+malinaalexa@users.noreply.github.com>
2023-01-17 01:45:49 +0200

    Add files via upload

--- comanda.h
+++ comanda.h
@@ -11,15 +11,14 @@
 class Comanda {
     std::vector<std::shared_ptr<Produs>> prod_comandate;
     double valoare=0;
-    int nrproduse;
+    int nrproduse=0;
 public:
     Comanda();
-    void swap(Comanda &obj1, Comanda obj2);
+    static void swap(Comanda &obj1, Comanda obj2);
     Comanda &operator=(const Comanda &obj);
     Comanda(const Comanda &other);
     explicit Comanda(const std::vector<std::shared_ptr<Produs>> &prodComandate);
     friend std::ostream &operator<<(std::ostream &os, const Comanda&comanda);
-
     void calculvaloare();
     void insert(const Produs &prod);
     virtual ~Comanda();
