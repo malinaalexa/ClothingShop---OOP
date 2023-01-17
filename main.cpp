47ee1058f4ffb2a75c31e6392c764ee068a1efcc
malinaalexa <104028370+malinaalexa@users.noreply.github.com>
2023-01-17 23:49:14 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,19 +10,17 @@
 int main() {
     Angajat An1("Popescu Ion", 2700, "Manager", 2, 20);
     std::cout<<An1.getsalariu()<<'\n';
-    std::vector<Produs*> Stoc;
+
     Bluza Bl{"Bluza de bumbac",99.99,15,0,"tricou"};
+
     try {
         Bl.setTip(Bl.gettip());
     } catch (std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
     }
     auto* st1 = dynamic_cast<Produs*>(&Bl);
-    if (st1 == nullptr)
-        std::cout << "null" << '\n';
     st1->reducere();
-    std::cout<<st1->pretprod();
-
+    std::cout<<st1->pretprod()<<'\n';
     Pantaloni Pan("Blugi Negri",180,15,0,"Scurti","Blugi");
     try {
         Pan.setTip(Pan.gettip());
@@ -35,9 +33,7 @@ int main() {
         std::cerr << "Error: " << e.what() << std::endl;
     }
     auto* st2 = dynamic_cast<Produs*>(&Pan);
-    if (st2 == nullptr)
-        std::cout << "null" << '\n';
-
+    std::cout<<st2->pretprod()<<'\n';
     Rochie Ro("Rochie roz",190.99,15,0,"Lunga");
     try {
         Ro.setLungime(Ro.getlungime());
@@ -45,9 +41,7 @@ int main() {
         std::cerr << "Error: " << e.what() << std::endl;
     }
     auto* st3 = dynamic_cast<Produs*>(&Ro);
-    if (st3 == nullptr)
-        std::cout << "null" << '\n';
-    
+    std::cout<<st3->pretprod()<<'\n';
     Comanda C1 = Comanda(std::vector<std::shared_ptr<Produs>>(), 0, An1,std::vector<int>());
     C1.insert(Bl,3);
     C1.insert(Pan,1);
